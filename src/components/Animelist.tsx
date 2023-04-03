23eed65e29d7f7aa5ecc95699feaaa288a1ad093
NataliyaChe <happylibrarygeek@gmail.com>
2023-04-03 22:26:27 +0300

    fetchAnimes & AnimeList fixed

--- src/components/Animelist.tsx
+++ src/components/Animelist.tsx
@@ -1,10 +1,18 @@
+import React from 'react';
 import { IAnimeCard } from '../interfaces/IAnimeCard';
 import AnimeCard from '../components/AnimeCard';
 
-function Animelist() {
+interface AnimelistProps {
+  animes: IAnimeCard[]
+}
+
+function Animelist({animes}: AnimelistProps) {
   return (
     <div className='grid grid-cols-5'>
-        {/* {animes.map(anime => <AnimeCard anime={anime} key={anime.mal_id}/>)} */}
+        {animes.map(anime => {
+          return <AnimeCard anime={anime} key={anime.mal_id} />
+        }
+      )}    
     </div>
   );
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from 'react';
-// import Animelist from '../components/Animelist';
+import Animelist from '../components/Animelist';
 import AnimeCard from '../components/AnimeCard';
 import { IAnimeCard } from '../interfaces/IAnimeCard';
 import Pagination from '../components/Pagination'
 
-// interface AnimeData {
-//   data: IAnimeCard[]
-//   pagination: {}
-// }
+interface AnimeData {
+  data: IAnimeCard[]
+  pagination: {
+    // current_page: number
+    items: {
+      // count: number
+      // per_page: number
+      total: number
+    }
+  }
+}
 
 export function Main() {
   const [animes, setAnimes] = useState<IAnimeCard[]>([]);
@@ -18,27 +25,35 @@ export function Main() {
   useEffect(() => {
     const fetchAnimes = async () => {
       const data = await fetch(`https://api.jikan.moe/v4/top/anime?limit=${animesPerPage}&page=${currentPage}`);
-      // const animes = await data.json() as AnimeData;
-      const animes = await data.json();
+      const animes = await data.json() as AnimeData;
+      // const animes = await data.json();
+      // console.log(animes);
       setTotalPages(animes.pagination.items.total);
       setAnimes(animes.data);
     }
     fetchAnimes()
   }, [[currentPage]])
 
-  // const changePage = (event: React.MouseEvent<HTMLButtonElement>) => {
-  // const changePage = (event: React.SyntheticEvent) => {
+  // const changePage = (event: React.MouseEvent<number>) => {
+  // const changePage = (event: React.BaseSyntheticEvent) => {
   const changePage = (event: any) => {
+    console.log('event', event);
+    
     setCurrentPage(event.selected+1)
   }
   // }
 
+  // function clickOnItem(name) {
+  //   console.log('name', name)
+  // }
+
   return (
     <>
-      <div className='grid grid-cols-5'>
+      {/* <div className='grid grid-cols-5'>
         {animes.map(anime => <AnimeCard anime={anime} key={anime.mal_id}/>)}
-      </div>
-      {/* <Animelist animes={animes} /> */}
+      </div> */}
+      <button onClick={changePage} >Test</button>
+      <Animelist animes={animes}/>
       <Pagination 
           changePage={changePage} 
           totalPages={totalPages}
