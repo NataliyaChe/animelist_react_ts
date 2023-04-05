79627625adf1cadef05173b6a2a7d3c81a0ae95a
NataliyaChe <happylibrarygeek@gmail.com>
2023-04-05 21:51:57 +0300

    single anime page style fixed

--- src/pages/SingleAnimePage.tsx
+++ src/pages/SingleAnimePage.tsx
@@ -18,29 +18,29 @@ export function SingleAnime() {
 
   return (
       <div className='p-8'>
-        <div className='flex gap-20 mb-6'>
+        <div className='flex gap-20 mb-6 text-lime-900'>
           <img 
             src={singleAnime?.images.webp.large_image_url} 
-            className='p-5 border-2 border-solid border-lime-600'/>
-          <div className=''>
+            className='p-5 border-2 border-solid border-lime-600 w-[300px]'/>
+          <div className='relative'>
             <h2 
-              className='text-3xl font-bold pb-6'>
+              className='text-3xl font-bold pb-6 text-lime-900'>
               {singleAnime?.title}</h2>
             <p 
-              className='text-xl font-semibold pb-4'>
+              className='text-xl font-semibold pb-4 text-lime-900'>
               Type: {singleAnime?.type}</p>
             <p 
-              className='text-xl font-semibold pb-4'>
+              className='text-xl font-semibold pb-4 text-lime-900'>
               Year: {singleAnime?.year}</p>
             <p 
-              className='text-xl font-semibold pb-4'>
+              className='text-xl font-semibold pb-4 text-lime-900'>
               Episodes: {singleAnime?.episodes}</p>
             <p 
-              className='text-xl font-semibold pb-4'>
+              className='text-xl font-semibold pb-4 text-lime-900'>
               Genres: {genres}</p>
           </div>
         </div> 
-        <p className='text-xl'>
+        <p className='text-xl absolute w-3/5 left-[420px]'>
           {singleAnime?.synopsis}</p>  
       </div>
   );
